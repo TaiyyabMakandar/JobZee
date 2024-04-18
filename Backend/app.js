504d7b9fda68d924745a8bbf5be642701906aa1a
Mohammad Taiyyab <100751807+TaiyyabMakandar@users.noreply.github.com>
2024-04-18 15:14:48 +0530

    Add files via upload

---
+++ Backend/app.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import jobRouter from "./routers/jobRouter.js";
+import userRouter from "./routers/userRouter.js";
+import applicationRouter from "./routers/applicationRouter.js";
+import { config } from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+
+
+// Rest of your code...
+
+
+
+
+
+const app = express();
+config({ path: "./config/config.env" });
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+}));
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/application", applicationRouter);
+app.use("/api/v1/job", jobRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
